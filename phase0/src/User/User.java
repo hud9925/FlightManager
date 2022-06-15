45c1776bdde14b82ad983189f578450acbc35e8b
KarlKKang <karl_kai.kang@icloud.com>
2022-06-15 03:42:35 -0400

    Partial implementation of 'User' class and 'UserTracker' class.

--- phase0/src/User/User.java
+++ phase0/src/User/User.java
@@ -1,4 +1,140 @@
 package User;
 
-public class User {
+import Ticket.Ticket;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+abstract public class User {
+    private final String username;
+    private String password;
+    private final int dob[];
+    private String email;
+    private boolean isMember;
+    private String address;
+    private List<Object> paymentMethods;
+    private Date lastLoginTime; // If the user just registered but have not logged in, lastLoginTime will be the register time.
+    private List<String[]> securityQuestions;
+    private List<Ticket> tickets;
+
+    // constructors
+    public User (String username, String password, int birthYear, int birthMonth, int birthDay, String email, boolean isMember) {
+        // To add other properties like address, security questions, payment methods etc., use corresponding setters.
+        this.username = username;
+        this.password = password;
+        this.dob= new int[] {birthYear, birthMonth, birthDay};
+        this.email = email;
+        this.isMember = isMember;
+
+        this.address = "";
+        this.paymentMethods = new ArrayList<Object>();
+        this.lastLoginTime = new Date();
+        this.securityQuestions = new ArrayList<String[]>();
+        this.tickets = new ArrayList<Ticket>();
+    }
+
+    // getters
+    public String getUsername () {
+        return this.username;
+    }
+
+    public String getPassword () {
+        return this.password;
+    }
+
+    public int getBirthDay () {
+        return this.dob[2];
+    }
+
+    public int getBirthMonth () {
+        return this.dob[1];
+    }
+
+    public int getBirthYear () {
+        return this.dob[0];
+    }
+
+    public String getEmail () {
+        return this.email;
+    }
+
+    public boolean getMemberStatus () {
+        return this.isMember;
+    }
+
+    public String getAddress () {
+        return this.address;
+    }
+
+    public Object getPaymentMethods () {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public Date getLastLoginTime () {
+        return new Date(this.lastLoginTime.getTime());
+    }
+
+    public String[][] getSecurityQuestions () {
+        String result[][] = new String[this.securityQuestions.size()][];
+        for (int i = 0; i < this.securityQuestions.size(); i++) {
+            result[i] = Arrays.copyOf(this.securityQuestions.get(i),2);
+        }
+        return result;
+    }
+
+    public Ticket[] getTickets () {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public abstract boolean isAdmin();
+
+    // setters
+    public void changePassword (String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void changeEmail (String newEmail) {
+        this.email = newEmail;
+    }
+
+    public void changeMemberStatus (boolean newStatus) {
+        this.isMember = newStatus;
+    }
+
+    public void changeAddress (String newAddress) {
+        this.address = newAddress;
+    }
+
+    public void addPaymentMethod (Object newMethod) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public void removePaymentMethod (Object oldMethod) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public void updateLastLoginTime () {
+        this.lastLoginTime = new Date();
+    }
+
+    public void addSecurityQuestion (String question, String answer) {
+        this.securityQuestions.add(new String[] {question, answer});
+    }
+
+    public void removeSecurityQuestion (int index) {
+        // Pre-condition: index < securityQuestions.size().
+        this.securityQuestions.remove(index);
+    }
+
+    public void addTicket (Ticket newTicket) {
+        // Pre-condition: 'newTicket' is not in the 'tickets' list.
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public void removeTicket (Ticket oldTicket) {
+        // Pre-condition: 'oldTicket' is in the 'tickets' list.
+        throw new UnsupportedOperationException("Not implemented");
+    }
 }
--- phase0/src/User/UserTracker.java
+++ phase0/src/User/UserTracker.java
@@ -1,4 +1,128 @@
 package User;
 
+import Ticket.Ticket;
+
+import java.util.List;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserTracker {
+    private static Map<String, User> allUsers = new HashMap<String, User>(); // HashMap for constant lookup time.
+
+    public static boolean userExists (String username) {
+        return allUsers.containsKey(username);
+    }
+
+    public static void addUser (User newUser) {
+        allUsers.put(newUser.getUsername(), newUser);
+    }
+
+    public static void removeUser (String username) {
+        allUsers.remove(username);
+    }
+
+    // Getters for basic user information
+    public static String getPassword (String username)  {
+        return allUsers.get(username).getPassword();
+    }
+
+    public static int getBirthDay (String username) {
+        return allUsers.get(username).getBirthDay();
+    }
+
+    public static int getBirthMonth (String username) {
+        return allUsers.get(username).getBirthMonth();
+    }
+
+    public static int getBirthYear (String username) {
+        return allUsers.get(username).getBirthYear();
+    }
+
+    public static String getEmail (String username) {
+        return allUsers.get(username).getEmail();
+    }
+
+    public static boolean getMemberStatus (String username) {
+        return allUsers.get(username).getMemberStatus();
+    }
+
+    public static String getAddress (String username) {
+        return allUsers.get(username).getAddress();
+    }
+
+    public static Object getPaymentMethods (String username) {
+        // Not yet implemented
+        // We need to decide how we store payment methods. Since there can be multiple payment methods, it's probably a list of some sort.
+        return allUsers.get(username).getPaymentMethods();
+    }
+
+    public static Date getLastLoginTime (String username) {
+        return allUsers.get(username).getLastLoginTime();
+    }
+
+    public static String[][] getSecurityQuestions (String username) {
+        return allUsers.get(username).getSecurityQuestions(); // String[][] { String[] {question, answer}, String[] {question, answer}, String[] {question, answer}, ... }
+    }
+
+    public static Ticket[] getTickets (String username) {
+        // Not yet implemented.
+        return allUsers.get(username).getTickets();
+    }
+
+    public static boolean isAdmin (String username) {
+        return allUsers.get(username).isAdmin();
+    }
+
+    // Setters for basic user information
+    public static void changePassword (String username, String newPassword)  {
+        allUsers.get(username).changePassword(newPassword);
+    }
+
+    public static void changeEmail (String username, String newEmail) {
+        allUsers.get(username).changeEmail(newEmail);
+    }
+
+    public static void changeMemberStatus (String username, boolean newStatus)  {
+        allUsers.get(username).changeMemberStatus(newStatus);
+    }
+
+    public static void changeAddress (String username, String newAddress)  {
+        allUsers.get(username).changeAddress(newAddress);
+    }
+
+    public static void addPaymentMethod (String username, Object newMethod) {
+        // Not yet implemented.
+        allUsers.get(username).addPaymentMethod(newMethod);
+    }
+
+    public static void removePaymentMethod (String username, Object oldMethod) {
+        // Not yet implemented.
+        allUsers.get(username).removePaymentMethod(oldMethod);
+    }
+
+    public static void updateLastLoginTime (String username) {
+        allUsers.get(username).updateLastLoginTime();
+    }
+
+    public static void addSecurityQuestion (String username, String question, String answer) {
+        allUsers.get(username).addSecurityQuestion(question, answer);
+    }
+
+    public static void removeSecurityQuestion (String username, int index) {
+        // Pre-condition: index < this.getSecurityQuestions(username).length.
+        allUsers.get(username).removeSecurityQuestion(index);
+    }
+
+    public static void addTicket (String username, Ticket newTicket) {
+        // Not yet implemented.
+        // Pre-condition: 'newTicket' is not in the user's 'tickets' list.
+        allUsers.get(username).addTicket(newTicket);
+    }
+
+    public static void removeTicket (String username, Ticket oldTicket) {
+        // Not yet implemented.
+        // Pre-condition: 'oldTicket' is in the user's 'tickets' list.
+        allUsers.get(username).removeTicket(oldTicket);
+    }
 }
