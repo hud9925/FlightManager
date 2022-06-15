1c0c23e3f9f09aa9e370670f2e8394d74f6dd9e4
Mani Setayesh <mani.setayesh@mail.utoronto.ca>
2022-06-15 17:32:10 -0400

    Done Customer and Admin Classes extending User

--- phase0/src/Entities/User/Admin.java
+++ phase0/src/Entities/User/Admin.java
@@ -1,4 +1,13 @@
 package Entities.User;
 
-public class Admin {
+public class Admin extends User{
+
+    public Admin(String username, String password, int birthYear, int birthMonth, int birthDay, String email, boolean isMember) {
+        super(username, password, birthYear, birthMonth, birthDay, email, isMember);
+    }
+
+    @Override
+    public boolean isAdmin() {
+        return true;
+    }
 }
--- phase0/src/Entities/User/Customer.java
+++ phase0/src/Entities/User/Customer.java
@@ -1,4 +1,13 @@
 package Entities.User;
 
-public class Customer {
+public class Customer extends User{
+
+    public Customer(String username, String password, int birthYear, int birthMonth, int birthDay, String email, boolean isMember) {
+        super(username, password, birthYear, birthMonth, birthDay, email, isMember);
+    }
+
+    @Override
+    public boolean isAdmin() {
+        return false;
+    }
 }
