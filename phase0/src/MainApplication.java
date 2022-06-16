aa53cb19572217600d11c968965c2bac0cd63743
Billy Luo <billy.luo@mail.utotonro.ca>
2022-06-16 14:58:11 -0400

    Partially completed code for login and register menus, and the main menu.

--- phase0/src/MainApplication.java
+++ phase0/src/MainApplication.java
@@ -1,5 +1,8 @@
+import ControllersAndPresenters.LoginMenu;
+
 public class MainApplication {
     public static void main(String[] args) {
-
+        // Call LoginMenu
+        LoginMenu.loginPage();
     }
 }
