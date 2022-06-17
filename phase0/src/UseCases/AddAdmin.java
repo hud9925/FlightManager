2a4720241235ce01ee065487773cecf1249df4ba
hulexian <Canada!578>
2022-06-17 18:04:09 -0400

    Added UseCase AddAdmin

--- phase0/src/UseCases/AddAdmin.java
+++ phase0/src/UseCases/AddAdmin.java
@@ -1,4 +1,15 @@
 package UseCases;
 
+import Entities.User.Admin;
+import Entities.User.UserTracker;
+
+/* The code below is used to add an admin User, and adds it to Usertracker
+ with the new User */
 public class AddAdmin {
+    public void NewAdmin (String username, String password, int birthYear, int birthMonth, int birthDay, String email, boolean isMember){
+//        Creates a new instance of admin
+        Admin NewAdmin = new Admin(username, password, birthYear, birthMonth, birthDay, email, isMember);
+        UserTracker.addUser(NewAdmin);
+    }
+
 }
