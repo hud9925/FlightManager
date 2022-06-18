d0162488d1649117c06bf9c9215b110c5765c30b
Mani Setayesh <mani.setayesh@mail.utoronto.ca>
2022-06-18 09:04:28 -0400

    Merged LoadFrom and Load classes into one gateway class, also added helper methods for Load class.

--- phase0/src/ControllersAndPresenters/LoadFrom.java
+++
@@ -1,4 +0,0 @@
-package ControllersAndPresenters;
-
-public class LoadFrom {
-}
--- phase0/src/UseCases/Load.java
+++ phase0/src/UseCases/Load.java
@@ -17,29 +17,35 @@ public class Load {
 
     public Load(BufferedReader br) throws IOException, ParseException {
         String line;
-        String[] userinfo;
-        String[] previousdates;
-        User newuser;
         while((line = br.readLine())!= null) {
-            userinfo = line.split(",");
-            if (userinfo[9].equals("true")){
-                newuser = new Admin(userinfo[0], userinfo[1], Integer.parseInt(userinfo[2]),
-                        Integer.parseInt(userinfo[3]), Integer.parseInt(userinfo[4]), userinfo[5],
-                        Boolean.parseBoolean(userinfo[6]));
-            } else {
-                newuser = new Customer(userinfo[0], userinfo[1], Integer.parseInt(userinfo[2]),
-                        Integer.parseInt(userinfo[3]), Integer.parseInt(userinfo[4]), userinfo[5],
-                        Boolean.parseBoolean(userinfo[6]));
-            }
-            newuser.changeAddress(userinfo[7]);
-            previousdates = userinfo[8].split("\\|");
-            List<Date> datelist = new ArrayList<>();
-            for(String str: previousdates){
-                Date d = new SimpleDateFormat("dd/MM/yyyy").parse(str);
-                datelist.add(d);
-            }
-            newuser.setDateList(datelist);
+            User newuser = lineToUser(line);
             UserTracker.addUser(newuser);
         }
+    }
+
+    public User lineToUser(String line) throws ParseException {
+        User newuser;
+        String [] userinfo = line.split(",");
+        if (userinfo[9].equals("true")){
+            newuser = new Admin(userinfo[0], userinfo[1], Integer.parseInt(userinfo[2]),
+                    Integer.parseInt(userinfo[3]), Integer.parseInt(userinfo[4]), userinfo[5],
+                    Boolean.parseBoolean(userinfo[6]));
+        } else {
+            newuser = new Customer(userinfo[0], userinfo[1], Integer.parseInt(userinfo[2]),
+                    Integer.parseInt(userinfo[3]), Integer.parseInt(userinfo[4]), userinfo[5],
+                    Boolean.parseBoolean(userinfo[6]));
+        }
+        newuser.changeAddress(userinfo[7]);
+        String[] previousdates = userinfo[8].split("\\|");
+        newuser.setDateList(loginDatesReader(previousdates));
+        return newuser;
+    }
+    public List<Date> loginDatesReader(String[] previousdates) throws ParseException {
+        List<Date> datelist = new ArrayList<>();
+        for(String str: previousdates){
+            Date d = new SimpleDateFormat("dd/MM/yyyy").parse(str);
+            datelist.add(d);
+        }
+        return datelist;
     }
 }
