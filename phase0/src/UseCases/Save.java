57abccb01f931aa340fa98cac925f979b7aac98b
Mani Setayesh <mani.setayesh@mail.utoronto.ca>
2022-06-18 08:56:22 -0400

    Merged SaveTo and Save classes into one gateway class

--- phase0/src/UseCases/Save.java
+++ phase0/src/UseCases/Save.java
@@ -3,15 +3,19 @@ package UseCases;
 import Entities.User.User;
 import Entities.User.UserTracker;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
 public class Save {
 
-    public Save(PrintWriter pw){
+    public Save() throws IOException {
+        PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\Owner\\Documents\\CSC207\\grouprepository\\phase0\\UserDatabase.csv"));
         Map<String, User> allusers = UserTracker.getAllUsers();
         for(User user : allusers.values()){
             pw.write(user.toString() + "\n");
         }
+        pw.close();
     }
 }
