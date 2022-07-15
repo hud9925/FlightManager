c5ecb742c5b85e13cdb9238e467a5de51e12e301
KarlKKang <karl_kai.kang@icloud.com>
2022-07-15 02:20:43 -0400

    Refactor database gateways: Reduce code duplication by making DatabaseConnector an abstract class instead of an interface

--- phase1/src/Gateways/FlightTrackerConnector.java
+++ phase1/src/Gateways/FlightTrackerConnector.java
@@ -6,53 +6,13 @@ import Entities.Flight.Seatmap;
 import UseCases.GetFlightList;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.Map;
 
-public class FlightTrackerConnector implements DatabaseConnector {
-
-    private final String filepath;
-
-    public FlightTrackerConnector() throws IOException{
-        String preferredPath = getAbsolutePath("FlightDatabase.txt");
-        if (fileExists(preferredPath)) {
-            this.filepath = preferredPath;
-            return;
-        }
-
-        String altPath = getAbsolutePath("phase1/FlightDatabase.txt");
-        if (fileExists(altPath)) {
-            this.filepath = altPath;
-            return;
-        }
-
-        this.filepath = preferredPath;
-        File preferredFile = new File(preferredPath);
-        if (!preferredFile.createNewFile()) {
-            throw new IOException();
-        }
-    }
-    /**
-     * Get the absolute path from a relative path.
-     *
-     * @param relPath The relative path to the working directory.
-     * @return The absolute path.
-     */
-    private String getAbsolutePath (String relPath) {
-        return String.valueOf(Paths.get(relPath).toAbsolutePath());
+public class FlightTrackerConnector extends DatabaseConnector {
+    public FlightTrackerConnector() throws IOException {
     }
 
-    /**
-     * Check if the file of a given path exists.
-     *
-     * @param path The path of the file to check.
-     * @return A boolean indicating whether the file exists.
-     */
-    private boolean fileExists (String path) {
-        File file = new File(path);
-        return file.isFile();
-    }
-
+    @Override
     public void Save() throws IOException {
         PrintWriter pw = new PrintWriter(new FileWriter(this.filepath));
         Map<String, Flight> allflights = GetFlightList.FlightMap();
@@ -61,6 +21,8 @@ public class FlightTrackerConnector implements DatabaseConnector {
         }
         pw.close();
     }
+
+    @Override
     public void Load() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(this.filepath));
         String line;
@@ -80,13 +42,13 @@ public class FlightTrackerConnector implements DatabaseConnector {
         return null;
     }
 
-    public Flight lineToFlight(String line, int row, int col) {
+    private Flight lineToFlight(String line, int row, int col) {
         // not yet implemented
         return null;
     }
 
-    public boolean checkEmpty() throws IOException {
-        BufferedReader file = new BufferedReader(new FileReader(this.filepath));
-        return file.readLine() == null;
+    @Override
+    protected String getDatabaseName () {
+        return "UserDatabase";
     }
 }
--- phase1/src/Gateways/UserTrackerConnector.java
+++ phase1/src/Gateways/UserTrackerConnector.java
@@ -6,66 +6,16 @@ import Entities.User.User;
 import Entities.User.UserTracker;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public class UserTrackerConnector implements DatabaseConnector {
-
-    private final String filepath;
-
-    /**
-     * Connect to the database.
-     * <p>
-     * This constructor will search for the 'UserDatabase.csv' file in the working directory first. If not found, it
-     * will search the 'phase1/' subdirectory of the working directory for that file. If the file is not found in either
-     * place, it will create this file in the working directory.
-     *
-     * @throws IOException when the file is not found in either place, and cannot be created.
-     */
-    public UserTrackerConnector() throws IOException{
-        String preferredPath = getAbsolutePath("UserDatabase.csv");
-        if (fileExists(preferredPath)) {
-            this.filepath = preferredPath;
-            return;
-        }
-
-        String altPath = getAbsolutePath("phase1/UserDatabase.csv");
-        if (fileExists(altPath)) {
-            this.filepath = altPath;
-            return;
-        }
-
-        this.filepath = preferredPath;
-        File preferredFile = new File(preferredPath);
-        if (!preferredFile.createNewFile()) {
-            throw new IOException();
-        }
+public class UserTrackerConnector extends DatabaseConnector {
+    public UserTrackerConnector() throws IOException {
     }
 
-    /**
-     * Get the absolute path from a relative path.
-     *
-     * @param relPath The relative path to the working directory.
-     * @return The absolute path.
-     */
-    private String getAbsolutePath (String relPath) {
-        return String.valueOf(Paths.get(relPath).toAbsolutePath());
-    }
-
-    /**
-     * Check if the file of a given path exists.
-     *
-     * @param path The path of the file to check.
-     * @return A boolean indicating whether the file exists.
-     */
-    private boolean fileExists (String path) {
-        File file = new File(path);
-        return file.isFile();
-    }
-
+    @Override
     public void Save() throws IOException {
         PrintWriter pw = new PrintWriter(new FileWriter(this.filepath));
         Map<String, User> allusers = UserTracker.getAllUsers();
@@ -74,6 +24,8 @@ public class UserTrackerConnector implements DatabaseConnector {
         }
         pw.close();
     }
+
+    @Override
     public void Load() throws IOException {
 //        String filepath = String.valueOf(Paths.get("UserDatabase.csv").toAbsolutePath());
         BufferedReader br = new BufferedReader(new FileReader(this.filepath));
@@ -85,7 +37,7 @@ public class UserTrackerConnector implements DatabaseConnector {
         br.close();
     }
 
-    public User lineToUser(String line) {
+    private User lineToUser(String line) {
         User newuser;
         String [] userinfo = line.split(",");
         if (userinfo[9].equals("true")){
@@ -104,7 +56,7 @@ public class UserTrackerConnector implements DatabaseConnector {
         }
         return newuser;
     }
-    public List<Date> loginDatesReader(String[] previousdates) {
+    private List<Date> loginDatesReader(String[] previousdates) {
         List<Date> datelist = new ArrayList<>();
         for(String str: previousdates){
             Date d = new Date(Long.parseLong(str));
@@ -113,9 +65,8 @@ public class UserTrackerConnector implements DatabaseConnector {
         return datelist;
     }
 
-    public boolean checkEmpty() throws IOException {
-//        String filepath = String.valueOf(Paths.get("UserDatabase.csv").toAbsolutePath());
-        BufferedReader file = new BufferedReader(new FileReader(this.filepath));
-        return file.readLine() == null;
+    @Override
+    protected String getDatabaseName () {
+        return "UserDatabase";
     }
 }
