61f3af756fac536137f641efd0c4278dd31199a3
Mani Setayesh <mani.setayesh@mail.utoronto.ca>
2022-07-15 16:25:10 -0400

    Implemented UserMenu and its controller Fixed String concatenation in loop issue in Seatmap using StringBuilder

--- phase1/src/Controllers/UserMenuC.java
+++ phase1/src/Controllers/UserMenuC.java
@@ -1,4 +1,31 @@
 package Controllers;
 
+import Presenters.AdminMenu;
+
+import java.io.IOException;
+
 public class UserMenuC {
+    public UserMenuC(String ans) throws IOException {
+        switch (ans){
+            case "1":
+                this.option1();
+                break;
+            case "2":
+                this.option2();
+                break;
+            default:
+                this.back();
+                break;
+        }
+    }
+
+    public void option1(){
+
+    }
+    public void option2(){
+
+    }
+    public void back() throws IOException {
+        AdminMenu.AdminPrompt();
+    }
 }
--- phase1/src/UseCases/GetUser.java
+++ phase1/src/UseCases/GetUser.java
@@ -5,10 +5,16 @@ import Entities.User.UserTracker;
 
 public class GetUser {
 
-    public GetUser() {}
-
     public User ReturnUser(String username){
         UserTracker ut = new UserTracker(username);
         return ut.getCurrentUser();
+    }
+
+    public String ReturnAllUsers(){
+       StringBuilder userlst = new StringBuilder();
+       for(User user : new UserTracker()){
+           userlst.append(user.toString()).append("\n");
+       }
+       return userlst.toString();
     }
 }
