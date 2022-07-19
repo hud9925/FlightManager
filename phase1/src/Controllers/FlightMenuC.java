723dae350dd5c88b6bfae9262016d65530004e25
Lexiang Hu <dan.hu@mail.utoronto.ca>
2022-07-18 23:24:03 -0400

    Added Complete FlightMenu Presenter, and incomplete implementation of FlightMenuC

--- phase1/src/Controllers/FlightMenuC.java
+++ phase1/src/Controllers/FlightMenuC.java
@@ -1,4 +1,19 @@
 package Controllers;
 
+import Presenters.FlightMenu;
+
+/**
+ * A Controller that calls upon a Presenter
+ * and sends the User's Input to PurchaseMenu
+ */
+
 public class FlightMenuC {
+    public FlightMenuC(String flightid){
+        FlightMenu.ShowDestinations();
+
+        //PurchaseMenu.PurchaseMenuPrompt(FlightMenu.SelectFlight());
+
+    }
+
+
 }
--- phase1/src/Presenters/FlightMenu.java
+++ phase1/src/Presenters/FlightMenu.java
@@ -1,10 +1,10 @@
 package Presenters;
+import java.util.HashMap;
 import java.util.Map;
 
 import Entities.Flight.Flight;
 import UseCases.FlightNotFoundException;
 import UseCases.GetFlightList;
-import UseCases.ShowFlight;
 
 /**
  * A Presenter Menu that Retrieves UseCase Information about Flights
@@ -13,16 +13,37 @@ import UseCases.ShowFlight;
 
 public class FlightMenu {
     /**
-     * Presents All Current Flights as a Map
+     * Presents All Available Flights(by their Id) with its Arrival Location, as a map
      */
-    public static Map<String, Flight> ShowFlights(){
-        return GetFlightList.FlightMap();
+    public static void ShowDestinations(){
+        Map<String, String> destinations = new HashMap<>();
+        for (Map.Entry<String, Flight> flight: GetFlightList.FlightMap().entrySet()){
+            String flightkey = flight.getKey();
+            String destination = flight.getValue().getArrivallocation();
+            destinations.put(flightkey, destination);
+        }
+        System.out.println("\n**********************************");
+        System.out.println("Here Are All The Current Flights and Their Destinations: ");
+        System.out.println("    Flight Number  "+ "    Flight Destination");
+        for (Map.Entry<String, String> flight: destinations.entrySet()){
+            System.out.println("    "+flight.getKey()+"    "+flight.getValue());
+        }
+        System.out.println("\n**********************************");
     }
     /**
      * Presents the Desired Flight with the given FlightiD
      */
-    public static Flight RetrieveFlight(String FlightId) throws FlightNotFoundException {
-        return ShowFlight.getFlight(FlightId);
+    public static String SelectFlight() {
+        return Console.prompt("Please enter the flight number of the flight you wish to view: ", "[A-Z]{2}[0-9]{3}");
     }
+
+    /**
+     * Presents All Current Flights as a Map
+     */
+    public static Map<String, Flight> ShowFlights(){
+        return GetFlightList.FlightMap();
+
+    }
+
 
 }
