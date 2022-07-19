8d468c1da3e9af2324bfa9f55051d0c1f255f4b9
Billy Luo <billy.luo@mail.utotonro.ca>
2022-07-19 17:07:02 -0400

    Add one line of code to redirect to CancelMenu presenter class after finishing calling CancelMenuC

--- phase1/src/Controllers/CancelMenuC.java
+++ phase1/src/Controllers/CancelMenuC.java
@@ -1,6 +1,7 @@
 package Controllers;
 
 import Entities.User.TicketNotFoundException;
+import Presenters.CancelMenu;
 import UseCases.CancelTicket;
 import UseCases.GetTicketList;
 import UseCases.GetUser;
@@ -9,5 +10,10 @@ public class CancelMenuC {
     public CancelMenuC(String username, int ticketIDHash) throws TicketNotFoundException {
         CancelTicket.CancelledTicket(GetUser.ReturnUser(username),
                 GetTicketList.getOneTicket(GetUser.ReturnUser(username), ticketIDHash));
+        boolean TicketNotFoundException = false;
+        if (!TicketNotFoundException){
+            System.out.println("Ticket has been successfully deleted! Redirect to the ticket menu...");
+        }
+        CancelMenu.CancelMenuPrompt();
     }
 }
