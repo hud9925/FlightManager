be70d1db753c5abb607cd1463f530540ad7343e0
Lexiang Hu <dan.hu@mail.utoronto.ca>
2022-07-18 23:20:42 -0400

    Added Method FlightInfo to UseCase GetFlightList; Method stores the Entity's Variable within a Hashmap

--- phase1/src/UseCases/GetFlightList.java
+++ phase1/src/UseCases/GetFlightList.java
@@ -3,7 +3,10 @@ package UseCases;
 import Entities.Flight.Flight;
 import Entities.Flight.FlightTracker;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Objects;
 
 // A Use Case that returns details pertaining to all current Flights including
 // the entire map of Flights; where the Integer keys of Flights Ids correspond with its associated Flight, as well as
@@ -22,5 +25,18 @@ public class GetFlightList {
      */
     public int NumFlights() {
         return FlightTracker.numFlights();
+    }
+    /**
+     Returns A Map Containing the Flight information(departure date, airline, etc)
+     */
+    public static Map<String, Object> FlightInfo(String id){
+        HashMap<String, Object> info = new HashMap<>();
+        info.put("Airline:", (Objects.requireNonNull(FlightTracker.getFlight(id))).getAirline());
+        info.put("Arrival Location:", Objects.requireNonNull(FlightTracker.getFlight(id)).getArrivallocation());
+        info.put("Departure Location:", Objects.requireNonNull(FlightTracker.getFlight(id)).getDeparturelocation());
+        info.put("Departure Date:", Objects.requireNonNull(FlightTracker.getFlight(id)).getDeparturedate());
+        info.put("Flight Duration:", Objects.requireNonNull(FlightTracker.getFlight(id)).getDuration());
+    return info;
+
     }
 }
