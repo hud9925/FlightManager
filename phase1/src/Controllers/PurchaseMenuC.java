2947b6bed381cdee7f402345e85408d21aba0066
Lexiang Hu <dan.hu@mail.utoronto.ca>
2022-07-19 18:21:59 -0400

    Filled in basic structure of PurchaseMenuC; incomplete implementation for now

--- phase1/src/Controllers/PurchaseMenuC.java
+++ phase1/src/Controllers/PurchaseMenuC.java
@@ -1,4 +1,28 @@
 package Controllers;
 
+
+import Entities.Flight.Flight;
+import Entities.Ticket.Ticket;
+import UseCases.GetUser;
+import UseCases.SeatViewer;
+import UseCases.BuyTicket;
+
+import java.util.Scanner;
+
 public class PurchaseMenuC {
+    public PurchaseMenuC(Flight flight, Integer row, Integer column){
+        //BuyTicket
+        // For Now --> No Cost
+        // Make User login again --> for now, only Takes Username
+        // create instance of ticket
+        Scanner ConfirmUser = new Scanner(System.in);
+        System.out.println("Please Enter Your Username again to continue");
+        String username = ConfirmUser.nextLine();
+
+        // some kind of bridging between Customer and User
+
+        new Ticket(flight, 0, GetUser.ReturnUser(username) , SeatViewer.getSeat(flight, row, column));
+        BuyTicket.PurchasedTicket()
+        }
+    }
 }
