0605e86fe425d837311763e762ccb01772ec04bf
Lexiang Hu <dan.hu@mail.utoronto.ca>
2022-07-21 15:10:56 -0400

    Added option of leaving the purchase ticket menu

--- phase1/src/Presenters/PurchaseMenu.java
+++ phase1/src/Presenters/PurchaseMenu.java
@@ -28,31 +28,38 @@ public class PurchaseMenu {
         System.out.println("Currently, Here is the Seating Chart. '-'s denote the Seat is Occupied." +
                 "Rows start from 0(VIP Row), while the letters identify the columns");
         System.out.println(SeatViewer.StringRepresentation(ShowFlight.getFlight(ans)));
+        String Ans  = Console.prompt("Please enter 'continue' to continue and purchase a ticket, or press any other character " +
+                "to leave this menu");
+        if (Objects.equals(Ans, "continue")){
+            System.out.println("To Purchase a seat, Please Select a Row and Column.");
 
-        System.out.println("To Purchase a seat, Please Select a Row and Column.");
+            Scanner SeatRow = new Scanner(System.in);
+            System.out.println("Please Enter the Seat's Row Number");
+            int row = SeatRow.nextInt();
+            Scanner SeatColumn = new Scanner(System.in);
+            System.out.println("Please Enter the Seat's Column Number");
+            //int column = SeatColumn.nextInt();
 
-        Scanner SeatRow = new Scanner(System.in);
-        System.out.println("Please Enter the Seat's Row Number");
-        int row = SeatRow.nextInt();
-        Scanner SeatColumn = new Scanner(System.in);
-        System.out.println("Please Enter the Seat's Column Number");
-        //int column = SeatColumn.nextInt();
+            char column = SeatColumn.next().charAt(0);
+            char CharRep = Character.toUpperCase(column);
 
-        char column = SeatColumn.next().charAt(0);
-        char CharRep = Character.toUpperCase(column);
+            if (SeatViewer.getSeat(ShowFlight.getFlight(ans), row, (int) CharRep -65)==null){
+                System.out.println("The Seat is not found on the plane. Please Try Again");
+                PurchaseMenuPrompt(ans);
+            }
+            else if (Objects.requireNonNull(SeatViewer.getSeat(ShowFlight.getFlight(ans), row, (int) CharRep -65)).isFilled()){
+                System.out.println(" ");
+                System.out.println("This Seat is already Occupied. Please Try Again");
+                System.out.println(" ");
+                PurchaseMenuPrompt(ans);
+            } else{
+                new PurchaseMenuC(ShowFlight.getFlight(ans), row, (int) CharRep -65);
+            }
 
-        if (SeatViewer.getSeat(ShowFlight.getFlight(ans), row, (int) CharRep -65)==null){
-            System.out.println("The Seat is not found on the plane. Please Try Again");
-            PurchaseMenuPrompt(ans);
-    }
-        else if (Objects.requireNonNull(SeatViewer.getSeat(ShowFlight.getFlight(ans), row, (int) CharRep -65)).isFilled()){
-            System.out.println(" ");
-            System.out.println("This Seat is already Occupied. Please Try Again");
-            System.out.println(" ");
-            PurchaseMenuPrompt(ans);
-        } else{
-            new PurchaseMenuC(ShowFlight.getFlight(ans), row, (int) CharRep -65);
+        } else {
+            new FlightMenu();
         }
+
     }
     public static String PurchaseAnotherTicket(){
         String ans = Console.prompt("Would You Like to Purchase Another Ticket? To Purchase Another ticket, please press 't,' or " +
