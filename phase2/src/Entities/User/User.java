2f54f225a245da6c396af54e50e9f9196386bc05
Mani Setayesh <mani.setayesh@mail.utoronto.ca>
2022-08-03 21:22:48 -0400

    Removed unused getters/setters/variables

--- phase2/src/Entities/User/User.java
+++ phase2/src/Entities/User/User.java
@@ -16,7 +16,7 @@ abstract public class User {
     /**
      * The password of this User.
      */
-    private String password;
+    private final String password;
     /**
      * The date of birth of this User, stored in the format int[] {birthYear, birthMonth, birthDay}.
      */
@@ -24,7 +24,7 @@ abstract public class User {
     /**
      * The email address of this User.
      */
-    private String email;
+    private final String email;
     /**
      * A boolean indicating whether this User is a member.
      */
@@ -43,12 +43,7 @@ abstract public class User {
      * A list of date when the User logged in.
      */
     private final List<Date> dateList;
-    /**
-     * A list of security questions for this User.
-     * <p>
-     * Each item in the list is in the format String[] {question, answer}.
-     */
-    private final List<String[]> securityQuestions;
+
     /**
      * A list of Tickets purchased by the User.
      */
@@ -75,7 +70,6 @@ abstract public class User {
         this.address = "";
         this.paymentMethods = new ArrayList<>();
         this.dateList = new ArrayList<>();
-        this.securityQuestions = new ArrayList<>();
         this.tickets = new ArrayList<>();
     }
 
@@ -88,8 +82,6 @@ abstract public class User {
         return this.username;
     }
 
-    public LocalDate getDob(){return this.dob;}
-
     /**
      * Get the password of this User.
      *
@@ -99,33 +91,6 @@ abstract public class User {
         return this.password;
     }
 
-
-    /**
-     * Get the email of this User.
-     *
-     * @return The email.
-     */
-    public String getEmail () {
-        return this.email;
-    }
-
-    /**
-     * Get if this User is a member or not.
-     *
-     * @return A boolean indicating whether this User is a member.
-     */
-    public boolean getMemberStatus () {
-        return this.isMember;
-    }
-
-    /**
-     * Get the address of this User.
-     *
-     * @return The address.
-     */
-    public String getAddress () {
-        return this.address;
-    }
 
     /**
      * Get the payment methods for this User.
@@ -153,19 +118,6 @@ abstract public class User {
         return result;
     }
 
-    /**
-     * Get an array of security questions for this User.
-     *
-     * @return An array of security questions in the format
-     * String[][] { String[] {question, answer}, String[] {question, answer}, String[] {question, answer}, ... }.
-     */
-    public String[][] getSecurityQuestions () {
-        String[][] result = new String[this.securityQuestions.size()][];
-        for (int i = 0; i < this.securityQuestions.size(); i++) {
-            result[i] = Arrays.copyOf(this.securityQuestions.get(i),2);
-        }
-        return result;
-    }
 
     /**
      * Get an array of tickets purchased by the user.
@@ -184,24 +136,6 @@ abstract public class User {
      * @return A boolean indicating whether this User is an admin.
      */
     public abstract boolean isAdmin();
-
-    /**
-     * Change the password of this User.
-     *
-     * @param newPassword The new password for this User.
-     */
-    public void changePassword (String newPassword) {
-        this.password = newPassword;
-    }
-
-    /**
-     * Change the email address of this User.
-     *
-     * @param newEmail The new email address for this User.
-     */
-    public void changeEmail (String newEmail) {
-        this.email = newEmail;
-    }
 
     /**
      * Set if this User is a member or not.
@@ -268,24 +202,6 @@ abstract public class User {
         }
     }
 
-    /**
-     * Add a security question to this User.
-     *
-     * @param question The question.
-     * @param answer The answer of the question.
-     */
-    public void addSecurityQuestion (String question, String answer) {
-        this.securityQuestions.add(new String[] {question, answer});
-    }
-
-    /**
-     * Remove a security question from this User.
-     *
-     * @param index The index of the question in the list returned by {@code this.getSecurityQuestions()}.
-     */
-    public void removeSecurityQuestion (int index) {
-        this.securityQuestions.remove(index);
-    }
 
     /**
      * Add a ticket to this User.
@@ -325,8 +241,7 @@ abstract public class User {
             String dateStr = String.valueOf(d.getTime());
             dateStrs.append(dateStr).append("|");
         }
-        return this.getUsername() + "," + this.getPassword() + "," + this.getDob()
-                + "," + this.getEmail() + "," + this.getMemberStatus()
-                + "," + this.getAddress() + "," + dateStrs + "," + (this.isAdmin()?"true":"false");
+        return username + "," + password + "," + dob  + "," + email + "," + isMember + "," + address + "," +
+                dateStrs + "," + (this.isAdmin()?"true":"false");
     }
 }
