a8fc765cf1a960cb95c575557b03bdbf26deb40b
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2022-08-04 02:26:47 -0400

    Added option to access MembershipMenu from the Main Menu

--- phase2/src/Presenters/Customer/MembershipMenu.java
+++ phase2/src/Presenters/Customer/MembershipMenu.java
@@ -5,4 +5,13 @@ package Presenters.Customer;
  */
 
 public class MembershipMenu {
+    /**
+     * Menu that presents the user's loyalty status and directs them to a list of
+     * member-only flights.
+     * If the user is not a member, they are prompted to either register for membership or go back
+     * to the main menu.
+     */
+    public static void MembershipMenuPrompt(){
+
+    }
 }
