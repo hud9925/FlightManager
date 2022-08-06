5f035c4846d26b31d04f13426e9c6ad2bbde47c1
Billy Luo <billy.luo@mail.utotonro.ca>
2022-08-06 11:53:54 -0400

    Enhanced the GUI "shell" appearance. Codes reserved for login system.

--- phase2/src/GUI/graphUI.java
+++ phase2/src/GUI/graphUI.java
@@ -1,36 +1,61 @@
 package GUI;
 import javax.swing.*;
-
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import java.awt.FlowLayout;
 
 public class graphUI {
 
+
     public static void main(String[] args) {
-            // Create and set up a frame window
+        JFrame f = new JFrame("Air Ticket Reserving System");
+        f.setSize(600, 600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JFrame frame = new JFrame("Air Ticket Reserving System");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel1 = new JPanel();
+        f.add(panel1);
+        firstPage(panel1);
+
+        f.setVisible(true);
+    }
+
+    private static void firstPage(JPanel panel1) {
+
+        panel1.setLayout(null);
+
+        JLabel title = new JLabel("Welcome to the Air Ticket Reserving System!");
+        title.setBounds(150,10,350,25);
+        panel1.add(title);
+
+        JButton newUserButton = new JButton("New User");
+        newUserButton.setBounds(100, 40, 150, 25);
+        panel1.add(newUserButton);
+
+        JButton existingUserButton = new JButton("Existing User");
+        existingUserButton.setBounds(280,40,150,25);
+        panel1.add(existingUserButton);
 
 
-            // Define new buttons
-        JLabel label = new JLabel("Welcome to the Air Ticket Reserving System...\n");
-        label.setBounds(50, 50, 100, 30);
-            JButton jb1 = new JButton("New User");
-            JButton jb2 = new JButton("Existing User");
-
-
-
-            // Define the panel to hold the buttons
-            JPanel panel = new JPanel();
-            panel.setLayout(new FlowLayout());
-            panel.add(jb1);
-            panel.add(jb2);
-            panel.add(label);
-        frame.setSize(500,300);
-
-            frame.add(panel);
-            frame.pack();
-            frame.setVisible(true);
-        }
-
+//        Reserved for login
+//        JLabel userLabel = new JLabel("User");
+//        userLabel.setBounds();
+//        panel1.add(userLabel);
+//
+//        JTextField userText = new JTextField(20);
+//        userText.setBounds();
+//        panel1.add(userText);
+//
+//        JLabel passwordLabel = new JLabel("Password");
+//        passwordLabel.setBounds();
+//        panel1.add(passwordLabel);
+//
+//        JPasswordField passwordText = new JPasswordField(20);
+//        passwordText.setBounds();
+//        panel1.add(passwordText);
+//
+    }
 }
