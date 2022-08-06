14e8aa32d764604439be7faeafa8e0689a4a9ed3
taymoorfarooq <taymoor.farooq@mail.utoronto.ca>
2022-08-06 15:45:58 -0400

    Added documentation for MembershipMenuC

--- phase2/src/Controllers/MembershipMenuC.java
+++ phase2/src/Controllers/MembershipMenuC.java
@@ -7,8 +7,19 @@ import Presenters.Customer.MembershipMenu;
 import UseCases.FlightNotFoundException;
 import UseCases.Admin.GetUser;
 
+/**
+ * Controller for the MembershipMenu presenter class that handles the options for non-member customers
+ * and redirects them accordingly.
+ */
 public class MembershipMenuC {
 
+    /**
+     * Takes the user's input from MembershipMenu. If they choose 1, they are sent to becomeMemberOption
+     * which changes their member status to member and redirects them to the MembershipMenu. If they choose 2, they
+     * are sent back to the MainMenu.
+     * @param choice the option chosen by the user.
+     * @param username the user's username.
+     */
     public MembershipMenuC(int choice, String username) throws FlightNotFoundException,
             TicketAlreadyExistsException, TicketNotFoundException {
         switch (choice){
@@ -21,17 +32,20 @@ public class MembershipMenuC {
         }
     }
 
-    public void becomeMemberOption(String username) throws FlightNotFoundException, TicketAlreadyExistsException,
-            TicketNotFoundException {
+    /**
+     * Option 1: Changes the user's membership status to member and redirects them to MembershipMenu.
+     * @param username the user's username.
+     */
+    public void becomeMemberOption(String username) throws FlightNotFoundException, TicketAlreadyExistsException, TicketNotFoundException {
         GetUser.returnUser(username).changeMemberStatus(true);
         System.out.println("Congratulations, you are now a member!");
         MembershipMenu.membershipMenuPrompt();
-
     }
 
+    /**
+     * Option 2: Redirects user to the main menu.
+     */
     public void mainMenuOption() throws FlightNotFoundException, TicketAlreadyExistsException, TicketNotFoundException {
         MainMenu.mainPage();
     }
-
 }
-
