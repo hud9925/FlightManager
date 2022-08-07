796f3faed17e467beebd63f572e90d6a54bf7e96
Lexiang Hu <dan.hu@mail.utoronto.ca>
2022-08-07 19:10:13 -0400

    Moved Login and Register tests into new test class

---
+++ phase2/test/CustomerUseCaseTests/LoginAndRegisterTests.java
@@ -0,0 +1,30 @@
+package CustomerUseCaseTests;
+
+import UseCases.Admin.AddAdmin;
+import UseCases.Customer.LoginUseCase;
+import UseCases.Customer.RegisterUseCase;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class LoginAndRegisterTests {
+    @Test(timeout = 50)
+    public void testLoginUseCase(){
+        AddAdmin.newAdmin("someone", "probably",  LocalDate.of(2000,10,20),
+                "amy");
+        ArrayList<Boolean> Predicates = new ArrayList<>();
+        Predicates.add(true);
+        Predicates.add(true);
+        assertEquals(Predicates, LoginUseCase.userType("someone", "probably"));
+    }
+    @Test(timeout = 50)
+    public void testRegisterUseCase(){
+        assertTrue(RegisterUseCase.newUser("C1", "P1", LocalDate.of(1111, 12, 21)
+                , "ma214"));
+        assertFalse(RegisterUseCase.newUser("C1", "P1", LocalDate.of(1111, 12, 21)
+                , "ma214"));
+    }
+}
