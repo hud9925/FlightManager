cba6a775d934b9070b7543bb9023df4e0eef7f79
Billy Luo <billy.luo@mail.utotonro.ca>
2022-08-08 11:56:55 -0400

    Added buttons for Admin/Main GUI pages

--- phase2/src/GUI/graphAdmin.java
+++ phase2/src/GUI/graphAdmin.java
@@ -9,19 +9,44 @@ public class graphAdmin {
     public static void setAdminPanel(){
         JPanel panelAdmin = new JPanel();
         f.add(panelAdmin);
-        mainPanelUI(panelAdmin);
+        adminPanelUI(panelAdmin);
         f.setVisible(true);
         statusLabel = new JLabel("");
         statusLabel.setBounds(30, 400, 500,25);
         panelAdmin.add(statusLabel);
     }
-    public static void mainPanelUI(JPanel panelAdmin){
+    public static void adminPanelUI(JPanel panelAdmin){
         panelAdmin.setLayout(null);
 
         JLabel title1 = new JLabel("Welcome to the Air Ticket Reserving System Admin Page!");
         title1.setBounds(120,10,380,25);
         panelAdmin.add(title1);
 
-    }
+        JLabel title2 = new JLabel("Select on of the following options to continue.");
+        title2.setBounds(120,40,380,25);
+        panelAdmin.add(title2);
 
+        JButton usersButton = new JButton("View all users");
+        usersButton.setBounds(100, 70, 150, 25);
+        panelAdmin.add(usersButton);
+        usersButton.addActionListener(e -> {
+            statusLabel.setText("Redirecting to the user list page... (not yet developed)");
+//            f.getContentPane().removeAll();
+//            f.repaint();
+
+        });
+
+        JButton flightsButton = new JButton("View the flights");
+        flightsButton.setBounds(300, 70, 150, 25);
+        panelAdmin.add(flightsButton);
+        flightsButton.addActionListener(e -> {
+            statusLabel.setText("Redirecting to the flight list page... (not yet developed)");
+//            f.getContentPane().removeAll();
+//            f.repaint();
+
+        });
+
+        graphMain.logout(panelAdmin);
+
+    }
 }
--- phase2/src/GUI/graphMain.java
+++ phase2/src/GUI/graphMain.java
@@ -22,5 +22,43 @@ public class graphMain {
         title1.setBounds(120,10,380,25);
         panelMain.add(title1);
 
+        JLabel title2 = new JLabel("Select on of the following options to continue.");
+        title2.setBounds(120,40,380,25);
+        panelMain.add(title2);
+
+//        JButton usersButton = new JButton("View all users");
+//        usersButton.setBounds(100, 70, 150, 25);
+//        panelMain.add(usersButton);
+//        usersButton.addActionListener(e -> {
+//            statusLabel.setText("Redirecting to the user list page... (not yet developed)");
+////            f.getContentPane().removeAll();
+////            f.repaint();
+//
+//        });
+//
+//        JButton flightsButton = new JButton("View the flights");
+//        flightsButton.setBounds(300, 70, 150, 25);
+//        panelMain.add(flightsButton);
+//        flightsButton.addActionListener(e -> {
+//            statusLabel.setText("Redirecting to the flight list page... (not yet developed)");
+////            f.getContentPane().removeAll();
+////            f.repaint();
+//
+//        });
+
+        logout(panelMain);
+
+
+    }
+
+    static void logout(JPanel somePanel) {
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setBounds(250, 130, 150, 25);
+        somePanel.add(logoutButton);
+        logoutButton.addActionListener(e -> {
+            f.getContentPane().removeAll();
+            f.repaint();
+            firstPage.setMainPage();
+        });
     }
 }
